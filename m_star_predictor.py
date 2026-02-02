453b8828e1ae1cdeecb58e4fa093757f11bd76da
Marta A. Mrozowska <mm6851@stellar-amd.princeton.edu>
2026-02-02 12:59:41 -0500

    added instantaneous, mean and realistic forcing predictors

--- m_star_predictor.py
+++ m_star_predictor.py
@@ -2,6 +2,43 @@ import torch
 from torch import nn
 import time
 
+from sklearn.model_selection import train_test_split
+
+
+def normalize(
+        data: torch.Tensor,
+        mean: torch.Tensor,
+        std: torch.Tensor
+):
+    return (data - mean) / std
+
+
+def real_units(
+        normalized_data: torch.Tensor,
+        mean: torch.Tensor,
+        std: torch.Tensor
+):
+    return normalized_data*std + mean
+
+
+def reduce_dataset(
+        X_data: torch.Tensor,
+        y_data: torch.Tensor,
+        random_state: int,
+        fraction: float = 0.1,
+):
+    _, X, _, y = train_test_split(
+        X_data, y_data, test_size=fraction, random_state=random_state
+    )
+    return X, y
+
+
+def compute_rmse(
+        y_pred: torch.Tensor,
+        y_true: torch.Tensor
+):
+    return (y_pred-y_true**2).mean().sqrt()
+
 
 class LinearRegression(nn.Module):
     def __init__(self, input_size=1):
@@ -12,6 +49,20 @@ class LinearRegression(nn.Module):
         # This method is automatically executed when
         # we call a object of this class
         x = self.linear1(x)
+        return x
+    
+
+class simple_FCNN(nn.Module):
+    def __init__(self, input_size=1):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, 8)
+        self.linear3 = nn.Linear(8, 1)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.relu(self.linear1(x))
+        x = self.linear3(x)
         return x
 
 
@@ -31,12 +82,14 @@ class FCNN(nn.Module):
         return x
     
 
-def train_model(network, criterion, loader, optimizer):
+def train_model(network, criterion, loader, optimizer, device):
     """Train the network for one epoch"""
+    network.to(device)
     network.train()
 
     train_loss = 0
     for batch_x, batch_y in loader:
+        batch_x, batch_y = batch_x.to(device), batch_y.to(device)
         # Get predictions
         if len(batch_x.shape) == 1:
             # This if block is needed to add a dummy dimension if our inputs are 1D
@@ -59,13 +112,14 @@ def train_model(network, criterion, loader, optimizer):
     return train_loss / len(loader)
 
 
-def test_model(network, criterion, loader):
+def test_model(network, criterion, loader, device):
     """Test the network"""
     network.eval()  # Evaluation mode (important when having dropout layers)
 
     test_loss = 0
     with torch.no_grad():
         for batch_x, batch_y in loader:
+            batch_x, batch_y = batch_x.to(device), batch_y.to(device)
             # Get predictions
             if len(batch_x.shape) == 1:
                 # This if block is needed to add a dummy dimension if our inputs are 1D
@@ -84,15 +138,16 @@ def test_model(network, criterion, loader):
     return test_loss
 
 
-def fit_model(network, criterion, optimizer, train_loader, test_loader, n_epochs):
+def fit_model(network, criterion, optimizer, train_loader, test_loader, n_epochs, device):
     """Train and validate the network"""
     train_losses, test_losses = [], []
     start_time = time.time()
     for epoch in range(1, n_epochs + 1):
-        train_loss = train_model(network, criterion, train_loader, optimizer)
-        test_loss = test_model(network, criterion, test_loader)
+        train_loss = train_model(network, criterion, train_loader, optimizer, device)
+        test_loss = test_model(network, criterion, test_loader, device)
         train_losses.append(train_loss)
         test_losses.append(test_loss)
+        print(f"epoch {epoch} completed")
     end_time = time.time()
     print(f"Training completed in {int(end_time - start_time)} seconds.")
 
